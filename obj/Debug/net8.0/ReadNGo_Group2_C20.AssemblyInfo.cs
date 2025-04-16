e60bcacbe33ec78ca51fcebfa18bbbcbd365f581
Sikum Hangma Madi <sikumhmadi@gmail.com>
2025-04-16 08:15:33 +0545

    Add backend code, cleaned .gitignore, removed build junk

--- obj/Debug/net8.0/ReadNGo_Group2_C20.AssemblyInfo.cs
+++ obj/Debug/net8.0/ReadNGo_Group2_C20.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReadNGo_Group2_C20")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b7b2b7dd0b8cdf468524cd843447e574cc49662")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReadNGo_Group2_C20")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReadNGo_Group2_C20")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
